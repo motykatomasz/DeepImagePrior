44a954516375e884b61489595a7c8ec136b4e4cd
ΛЯK <arkspeed@gmail.com>
2020-03-21 14:04:14 +0100

    Updated tran.py

---
+++ models/utils.py
@@ -0,0 +1,45 @@
+import math
+import torch
+from torchvision.transforms import Compose, ToPILImage, ToTensor
+from PIL import Image
+import numpy as np
+
+use_gpu = torch.cuda.is_available()
+
+def get_padding_by_kernel(kernel_size):
+    # Assuming we are using odd kernel size
+    padding_size = math.floor(kernel_size/2)
+    return padding_size, padding_size
+
+
+def z(shape, t='random', channels=1):
+    if t=='random':
+        return 0.1 * np.random.random((1,channels) + shape)
+    if t=='meshgrid':
+        result = np.zeros((1,2) + shape)
+        result[0, 0, :, :], result[0, 1, :, :] = np.meshgrid(
+            np.linspace(0,1,shape[0]),
+            np.linspace(0,1,shape[1])
+        )
+        return result
+
+
+def image_to_tensor(filepath):
+    # accept a file path to an image, return a torch tensor
+    pil = Image.open(filepath)
+    pil_to_tensor = Compose([ToTensor()])
+    if use_gpu:
+        tensor = pil_to_tensor(pil).cuda()
+    else:
+        tensor = pil_to_tensor(pil)
+    return tensor.view([1]+list(tensor.shape))
+
+
+def tensor_to_image(tensor, filename):
+    # accept a torch tensor, convert it to an image at a certain path
+    tensor = tensor.view(tensor.shape[1:])
+    if use_gpu:
+        tensor = tensor.cpu()
+    tensor_to_pil = Compose([ToPILImage()])
+    pil = tensor_to_pil(tensor)
+    pil.save(filename)
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
-from torchvision.transforms import Compose, ToPILImage, ToTensor
+from .models.utils import z
 
 
 def imshow(img):
@@ -44,13 +44,11 @@ out = net(x)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
+# Num of iters for training
 num_iters = 1000
 
 # Num of iters when to save image
 save_frequency = 250
-
-#blackout pixels' proportion in percentage
-prop = 0.5
 
 #Since we only have 1 image to train on, we set zero_gradienet once at the beginning
 optimizer.zero_grad()
@@ -58,55 +56,18 @@ optimizer.zero_grad()
 # Network Model
 net = UNet()
 
-
-def image_to_tensor(filepath=img_path):
-    # accept a file path to an image, return a torch tensor
-    pil = Image.open(filepath)
-    pil_to_tensor = Compose([ToTensor()])
-    if use_gpu:
-        tensor = pil_to_tensor(pil).cuda()
-    else:
-        tensor = pil_to_tensor(pil)
-    return tensor.view([1]+list(tensor.shape))
-
-
-def tensor_to_image(tensor, filename):
-    # accept a torch tensor, convert it to an image at a certain path
-    tensor = tensor.view(tensor.shape[1:])
-    if use_gpu:
-        tensor = tensor.cpu()
-    tensor_to_pil = Compose([ToPILImage()])
-    pil = tensor_to_pil(tensor)
-    pil.save(filename)
-
-
-def zero_out_pixels(tensor, prop=prop):
-    # function which zeros out a random proportion of pixels from an image tensor.
-    if use_gpu:
-        mask = torch.rand([1]+[1] + list(tensor.shape[2:])).cuda()
-    else:
-        mask = torch.rand([1]+[1] + list(tensor.shape[2:]))
-    mask[mask<prop] = 0
-    mask[mask!=0] = 1
-    mask = mask.repeat(1,3,1,1)
-    deconstructed = tensor * mask
-    return mask, deconstructed
-
-
 for iter in range(num_iters):
-
+    input = z(shape=[img.height, img.width])
     output = net(x)
 
-    mask, deconstructed = zero_out_pixels(image_to_tensor(img_path))
     mask_output = output * mask
 
     # Optimizer
-    loss = torch.sum((mask_output - deconstructed)**2)
+    loss = torch.sum((mask_output - output)**2)
     loss.backward()
     optimizer.step()
 
     print('Step :{}, Loss: {}'.format(iter, loss.data.cpu()))
-    
-    #ToDo
+
     if num_iters % save_frequency == 0:
         print('OUTPUT IMAGE')
