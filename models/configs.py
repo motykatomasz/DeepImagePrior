44e747d6484e7b0b93df4a0dc7bbfda08b6a85ce
Andrea Nardi <buongiorno19972@gmail.com>
2020-03-28 14:31:16 +0100

    refactored model and added sigmoid at the end

--- models/configs.py
+++ models/configs.py
@@ -24,6 +24,16 @@ superresolutionSettings = {
 }
 
 
+textInpaintingSettings = {
+    "channels_down": [128, 128, 128, 128, 128],
+    "channels_up": [128, 128, 128, 128, 128],
+    "channels_skip": [4, 4, 4, 4, 4],
+    "kernels_down": [3, 3, 3, 3, 3],
+    "kernels_up": [3, 3, 3, 3, 3],
+    "kernels_skip": [1, 1, 1, 1, 1],
+    "upsampling_method": "bilinear",
+}
+
 testSettings = {
     "channels_down": [3, 16, 32, 64, 128],
     "channels_up": [128, 64, 32, 16, 3],
