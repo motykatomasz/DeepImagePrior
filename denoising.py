e3794be748d3b3116f6fd7e8a02f1028bc344e10
Tomasz <motyka.tomasz.1995@gmail.com>
2020-04-08 15:26:06 +0200

    Test denoising. Add small change with output convolution.

--- denoising.py
+++ denoising.py
@@ -4,23 +4,22 @@ from PIL import Image
 from numpy import asarray
 from models.unet import UNet
 import torch.optim as optim
-from models.utils import z, imshow, image_to_tensor, tensor_to_image
-from models.configs import textInpaintingSettings
+from models.utils import z, imshow, image_to_tensor, tensor_to_image, get_noisy_image, numpy_to_tensor, image_to_numpy
+from models.configs import denoisingSettings
 
-img_path = "data/inpainting/kate.png"
+img_path = "data/denoising/F16_GT.png"
 img = Image.open(img_path)
-imshow(asarray(img))
+img_np = asarray(img)
+imshow(img_np)
 
-mask_path = "data/inpainting/kate_mask.png"
-mask = Image.open(mask_path)
-imshow(asarray(mask))
+sigma = 1./10.
+noisy_img = get_noisy_image(image_to_numpy(img), sigma)
 
-x = image_to_tensor(img)
-mask = image_to_tensor(mask)
+x = numpy_to_tensor(noisy_img)
 
-img_masked = x * mask
+imshow(asarray(tensor_to_image(x)))
 
-net = UNet(32, textInpaintingSettings)
+net = UNet(3, denoisingSettings)
 
 if torch.cuda.is_available():
     net = net.cuda()
@@ -29,37 +28,37 @@ mse = torch.nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=0.01)
 
 # Num of iters for training
-num_iters = 5000
+num_iters = 2400
 
 # Num of iters when to save image
 save_frequency = 50
 
-z0 = z(shape=(img.height, img.width), channels=32)
+z0 = z(shape=(img.height, img.width), channels=3)
+
+out_avg = None
 z0_saved = z0.detach().clone()
 noise = z0.detach().clone()
-z0_noise_std = 0.03
+z0_noise_std = 1./30.
 
 last_lost = None
 last_model_state = net.state_dict()
 
 for i in range(num_iters):
+    optimizer.zero_grad()
 
     if z0_noise_std > 0:
         z0 = z0_saved + (noise.normal_() * z0_noise_std)
 
-    optimizer.zero_grad()
     output = net(z0)
 
     # Optimizer
-    # loss = torch.sum(torch.mul((output - x), mask)**2)
-    loss = mse(output * mask, x * mask)
-    # loss = mse(output * mask, img_masked)
+    loss = mse(output, x)
     loss.backward()
     optimizer.step()
 
     cpu_loss = loss.data.cpu()
-    print('Step :{}, Loss: {}'.format(i, cpu_loss))
     if i % save_frequency == 0:
+        print('Step :{}, Loss: {}'.format(i, loss.data.cpu()))
         out_img = tensor_to_image(output)
         imshow(asarray(out_img))
         print('OUTPUT IMAGE')
